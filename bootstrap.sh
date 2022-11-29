331d7d81925bfce3df1bdabee130a19c2905c7d9
steven0323 <Steph199603@gmail.com>
2022-11-28 20:47:32 -0800

    reformat

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,31 +4,31 @@
 echo "Start bootstrap ... \n"
 
 clone_ccil(){
-    echo "cloning ccil_vwd ... \n"
+		echo "cloning ccil_vwd ... \n"
     
-	git clone git@github.com:jyadams/ccil_vwd.git
+		git clone git@github.com:jyadams/ccil_vwd.git
 	
-	echo "ccil_vwd download finished !! \n"
+		echo "ccil_vwd download finished !! \n"
 }
 
 clone_ventmap(){
-    echo "cloning ventMAP ... \n" 
+		echo "cloning ventMAP ... \n" 
 	
-	git clone git@github.com:yu-hsuan-tseng/ventMAP.git
+		git clone git@github.com:yu-hsuan-tseng/ventMAP.git
 	
-	echo "ventMAP download finished !! \n"
+		echo "ventMAP download finished !! \n"
 }
 
 transfer_data(){
-	mkdir -p ccil_vwd/dataset/experiment1/all_data/raw
+		mkdir -p ccil_vwd/dataset/experiment1/all_data/raw
     
-	echo "transferring data from database ... \n"
+		echo "transferring data from database ... \n"
     
-	cd ccil_vwd
-	source activate ards
-	python transfer_data.py --data-path ccil_vwd/dataset/experiment1/all_data/raw/ --cohort-description ccil_vwd/dataset/cohort-description.csv
+		cd ccil_vwd
+		source activate ards
+		python transfer_data.py --data-path dataset/experiment1/all_data/raw/ --cohort-description dataset/cohort-description.csv
 
-	echo "transferring data finished !! \n"
+		echo "transferring data finished !! \n"
 }
 
 clone_ccil
