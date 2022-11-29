7896da1c7eb9910852eddcb9183c42d6d03280ca
YU-HSUAN TSENG <38046774+yu-hsuan-tseng@users.noreply.github.com>
2022-11-28 20:56:53 -0800

    formatting

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,24 @@
 echo "Start bootstrap ..."
 
 clone_ccil(){
-  echo "cloning ccil_vwd ..."
-  git clone git@github.com:jyadams/ccil_vwd.git
-	echo "ccil_vwd download finished !!"
+    echo "cloning ccil_vwd ..."
+    git clone git@github.com:jyadams/ccil_vwd.git
+    echo "ccil_vwd download finished !!"
 }
 
 clone_ventmap(){
-  echo "cloning ventMAP ..." 
-	git clone git@github.com:yu-hsuan-tseng/ventMAP.git
-	echo "ventMAP download finished !!"
+    echo "cloning ventMAP ..." 
+    git clone git@github.com:yu-hsuan-tseng/ventMAP.git
+    echo "ventMAP download finished !!"
 }
 
 transfer_data(){
-	mkdir -p ccil_vwd/dataset/experiment1/all_data/raw
-  echo "transferring data from database ..."
-  cd ccil_vwd
-	source activate ards
-	python transfer_data.py --data-path dataset/experiment1/all_data/raw/ --cohort-description dataset/cohort-description.csv
-  echo "transferring data finished !!"
+    mkdir -p ccil_vwd/dataset/experiment1/all_data/raw
+    echo "transferring data from database ..."
+    cd ccil_vwd
+    source activate ards
+    python transfer_data.py --data-path dataset/experiment1/all_data/raw/ --cohort-description dataset/cohort-description.csv
+    echo "transferring data finished !!"
 }
 
 clone_ccil
