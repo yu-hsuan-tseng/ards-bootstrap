e20dcbbe0a1166d3f01cf7c8f685cdb55e269fc7
steven0323 <Steph199603@gmail.com>
2022-11-28 20:55:14 -0800

    reformat

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 # Bootstrap will clone all the repository needed for you 
 
-echo "Start bootstrap ... \n"
+echo "Start bootstrap ..."
 
 clone_ccil(){
-  echo "cloning ccil_vwd ... \n"
+  echo "cloning ccil_vwd ..."
   git clone git@github.com:jyadams/ccil_vwd.git
-	echo "ccil_vwd download finished !! \n"
+	echo "ccil_vwd download finished !!"
 }
 
 clone_ventmap(){
-  echo "cloning ventMAP ... \n" 
+  echo "cloning ventMAP ..." 
 	git clone git@github.com:yu-hsuan-tseng/ventMAP.git
-	echo "ventMAP download finished !! \n"
+	echo "ventMAP download finished !!"
 }
 
 transfer_data(){
 	mkdir -p ccil_vwd/dataset/experiment1/all_data/raw
-  echo "transferring data from database ... \n"
+  echo "transferring data from database ..."
   cd ccil_vwd
 	source activate ards
 	python transfer_data.py --data-path dataset/experiment1/all_data/raw/ --cohort-description dataset/cohort-description.csv
-  echo "transferring data finished !! \n"
+  echo "transferring data finished !!"
 }
 
 clone_ccil
