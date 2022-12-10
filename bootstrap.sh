71c265bc256c525431d29cd7ff3ad280c10b69bc
Steven0323 <Steph199603@gmail.com>
2022-12-09 18:47:50 -0800

    predefine anonymize_data

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ echo "Start ards bootstrap ..."
 clone_ccil(){
     # TODO ccil_vwd is a private repository, we might need to pre-configure your git to fullfill that
     echo "cloning ccil_vwd ..." 
+    cd ..
     git clone git@github.com:jyadams/ccil_vwd.git
     echo "ccil_vwd download finished !!"
 }
@@ -27,8 +28,16 @@ transfer_data(){
     echo "transferring data finished !!"
 }
 
+anonymize_data(){
+    #TODO Combine anonymize_data.sh in here
+    echo "Please specify your data path in ventMAP/default.yaml"
+    cd ../ventMAP 
+    sh anonymize_data.sh 
+}
+
 clone_ccil
 clone_ventmap
 transfer_data
+# anonymize_data
 
 echo "Boostrap finished !!"
