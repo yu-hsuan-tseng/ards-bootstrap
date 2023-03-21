45f104dfeb44c00ab9c5f3a1d8b6408716b3a6fc
Steven0323 <Steph199603@gmail.com>
2023-03-20 21:55:46 -0700

    add flexibility

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,17 +7,28 @@ echo "Start ards bootstrap ..."
 # Clone ccil_vwd repo to run transfer_data.py
 clone_ccil(){
     # TODO ccil_vwd is a private repository, we might need to pre-configure your git to fulfill that
-    echo "cloning ccil_vwd ..." 
-    cd ..
-    git clone git@github.com:jyadams/ccil_vwd.git
-    echo "ccil_vwd download finished !!"
+    if [ -d "../ccil_vwd" ]
+    then
+      echo "ccil_vwd already exists"
+      cd ..
+    else
+      echo "cloning ccil_vwd ..." 
+      cd ..
+      git clone git@github.com:jyadams/ccil_vwd.git
+      echo "ccil_vwd download finished !!"
+    fi
 }
 
 # Clone ventMap software to run data preprocessing
 clone_ventmap(){
-    echo "cloning ventMAP ..." 
-    git clone git@github.com:yu-hsuan-tseng/ventMAP.git
-    echo "ventMAP download finished !!"
+    if [ -d "ventMAP" ] 
+    then
+      echo "ventMap already exists"
+    else
+      echo "cloning ventMAP ..." 
+      git clone git@github.com:yu-hsuan-tseng/ventMAP.git
+      echo "ventMAP download finished !!"
+    fi
 }
 
 # Transfer data from server to local
@@ -37,8 +48,13 @@ transfer_data(){
     # go back to ardsdetection code
     cd ../ccil_vwd
     
-    mkdir -p anon_dataset/experiment1/all_data/raw
-    mkdir -p unanon_dataset/experiment1/all_data/raw
+    if [ ! -d "anon_dataset" ] && [ ! -d "unanon_dataset" ]
+    then
+      mkdir -p anon_dataset/experiment1/all_data/raw
+      mkdir -p unanon_dataset/experiment1/all_data/raw
+    else
+      echo "folder already exists"
+    fi
     mv ../cohort-description.csv unanon_dataset/
     source activate ards
     python transfer_data.py --data-path unanon_dataset/experiment1/all_data/raw/ --cohort-description unanon_dataset/cohort-description.csv
